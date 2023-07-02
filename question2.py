c7e9f3eaaeedb9b9ec20a9961d302101124c383f
BatuhanAyyildizz <batin.1bey@gmail.com>
2023-07-02 17:10:12 +0300

    HackerRank questions python

---
+++ question2.py
@@ -0,0 +1,10 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    
+    summ= a+b
+    diff = a-b
+    multi = a*b
+    print(str(summ))
+    print( str(diff))
+    print( str(multi))
